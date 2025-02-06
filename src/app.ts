cb64f0bf0103f99fceeb6708616bff3af9b98b1b
Maria Korol <mariakorol@Marias-MacBook-Air.local>
2025-02-06 17:42:22 +0300

    fix: linting

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,20 @@
 import express, { Request, Response, NextFunction } from 'express';
 import mongoose from 'mongoose';
-import userRouter from "./routes/users";
-import cardRouter from "./routes/cards";
-import {IAppError, IInfoRequest} from "./interface";
+import userRouter from './routes/users';
+import cardRouter from './routes/cards';
+import { IAppError, IInfoRequest } from './interface';
 
-const { PORT = 3000, BASE_PATH } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb').then(()=> {});
+mongoose.connect('mongodb://localhost:27017/mestodb').then(() => {});
 
 app.use((req: IInfoRequest, res: Response, next: NextFunction) => {
-    req.user = {
-        _id: '67a47e7c0d5682a0f923ae16'
-    };
+  req.user = {
+    _id: '67a47e7c0d5682a0f923ae16',
+  };
 
-    next();
+  next();
 });
 
 app.use(express.json());
@@ -23,15 +23,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(userRouter);
 app.use(cardRouter);
 
-app.use((err: IAppError, req: Request, res: Response, next: NextFunction) => {
-    const { statusCode, message, name } = err;
+app.use((err: IAppError, req: Request, res: Response) => {
+  const { statusCode, message, name } = err;
 
-    if (name === 'CastError') {
-        res.status(400).send({ message: "Переданы некорректные данные" });
-        return;
-    }
+  if (name === 'CastError') {
+    res.status(400).send({ message: 'Переданы некорректные данные' });
+    return;
+  }
 
-    res.status(statusCode || 500).send({ message: statusCode === 500 ? 'На сервере произошла ошибка!' : message });
+  res.status(statusCode || 500).send({ message: statusCode === 500 ? 'На сервере произошла ошибка!' : message });
 });
 
 app.listen(PORT);
