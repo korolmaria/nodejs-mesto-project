5c4dcc8e30afb7fc4e9bbacf5316e4ee4957e94e
Maria Korol <mariakorol@Marias-MacBook-Air.local>
2025-02-09 14:11:44 +0300

    fix: add validation in cards roter for cardId param

--- src/routes/cards.ts
+++ src/routes/cards.ts
@@ -26,19 +26,19 @@ router.post('/cards', celebrate({
 
 router.delete('/cards/:cardId', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().length(24),
+    cardId: Joi.string().length(24).hex().required(),
   }),
 }), deleteCardById);
 
 router.put('/cards/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().length(24),
+    cardId: Joi.string().length(24).hex().required(),
   }),
 }), likeCard);
 
 router.delete('/cards/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().length(24),
+    cardId: Joi.string().length(24).hex().required(),
   }),
 }), dislikeCard);
 
